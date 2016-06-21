67c6be93f6e3cd67f9b909c21acc4ed06a833d29
Kim Gunnarsson <kim@morgondag.nu>
2016-06-21 22:01:34 +0200

    basic setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# web-morgondag
+[![Build Status](https://travis-ci.org/MorgondagDev/web-kim.svg?branch=master)](https://travis-ci.org/MorgondagDev/web-kim)
+
+# web-kim
+==============
---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	componentDidMount(){
+
+	}
+	render(){
+		return(
+			<article>
+				<h1>Morgondag</h1>
+			</article>
+		)
+	}
+}
