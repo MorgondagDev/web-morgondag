d9f930765543b40402dd8a38fa2623264aa98f20
Kim Gunnarsson <kim@morgondag.nu>
2016-07-25 10:56:53 +0200

    webpack path config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -27,7 +27,7 @@ module.exports = {
     }, {
       test: /.*\.(gif|png|jpe?g|svg|ico)$/i,
       loaders: [
-        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'file?name=img/[name].[ext]',
         'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
       ]
     }, {
